2d0dfc7a02f496c9b7038f6693365e0712da3433
DimMilios <dimitris.milios96@gmail.com>
2023-06-02 17:09:52 +0300

    fix db dialect, populate db if empty

--- src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/guru/springframework/msscbeerservice/bootstrap/BeerLoader.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-05-17.
  */
-//@Component
+@Component
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -34,33 +34,32 @@ public class BeerLoader implements CommandLineRunner {
 
     private void loadBeerObjects() {
         if(beerRepository.count() == 0){
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_1_UPC)
+                    .price(new BigDecimal("12.95"))
+                    .build());
 
-//            beerRepository.save(Beer.builder()
-//                    .beerName("Mango Bobs")
-//                    .beerStyle("IPA")
-//                    .quantityToBrew(200)
-//                    .minOnHand(12)
-//                    .upc(BEER_1_UPC)
-//                    .price(new BigDecimal("12.95"))
-//                    .build());
-//
-//            beerRepository.save(Beer.builder()
-//                    .beerName("Galaxy Cat")
-//                    .beerStyle("PALE_ALE")
-//                    .quantityToBrew(200)
-//                    .minOnHand(12)
-//                    .upc(BEER_2_UPC)
-//                    .price(new BigDecimal("11.95"))
-//                    .build());
-//
-//            beerRepository.save(Beer.builder()
-//                    .beerName("No Hammers On The Bar")
-//                    .beerStyle("PALE_ALE")
-//                    .quantityToBrew(200)
-//                    .minOnHand(12)
-//                    .upc(BEER_3_UPC)
-//                    .price(new BigDecimal("11.95"))
-//                    .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_2_UPC)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("No Hammers On The Bar")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(BEER_3_UPC)
+                    .price(new BigDecimal("11.95"))
+                    .build());
         }
     }
 }
